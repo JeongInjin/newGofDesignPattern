bbae68d12aaf8a4a332d85036fec3ae1729ca7c9
injinjeong <snowwwwww@naver.com>
2022-12-12 18:33:02 +0900

    first commit

---
+++ src/main/java/com/example/new_gof_design_pattern/NewGofDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_gof_design_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGofDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewGofDesignPatternApplication.class, args);
+    }
+
+}
