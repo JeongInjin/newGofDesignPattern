4f239666e07de4442b1db5f996209b5ecb2672f2
injinjeong <snowwwwww@naver.com>
2023-01-02 15:05:32 +0900

    Iterator Pattern

---
+++ src/main/java/com/example/new_gof_design_pattern/_10_iterator/hfd/dinermergercafe/Menu.java
@@ -0,0 +1,7 @@
+package com.example.new_gof_design_pattern._10_iterator.hfd.dinermergercafe;
+
+import java.util.Iterator;
+
+public interface Menu {
+	public Iterator<MenuItem> createIterator();
+}
